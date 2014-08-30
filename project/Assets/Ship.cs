7b7e56bbeaac0647ee6adb8f2344f5b2c6fe8ed8
Robinerd Laptop <robinerd@gmail.com>
2014-08-30 22:22:00 +0200

    steering

---
+++ project/Assets/Ship.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+	public float force;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			Debug.Log("hnnngg");
+			rigidbody2D.AddForceAtPosition(
+				transform.up * force * Time.deltaTime, transform.TransformPoint(0.25f,0,0));
+		}
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			rigidbody2D.AddForceAtPosition(
+				transform.up * force * Time.deltaTime, transform.TransformPoint(-0.25f,0,0));
+		}
+	}
+}
