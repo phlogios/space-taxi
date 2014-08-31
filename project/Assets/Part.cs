7547bec3f2a999e88fc365fafd08640e97030a09
Robinerd Laptop <robinerd@gmail.com>
2014-08-31 10:04:20 +0200

    HP + visible changes

--- project/Assets/Part.cs
+++ project/Assets/Part.cs
@@ -2,16 +2,17 @@
 using System.Collections;
 
 public class Part : MonoBehaviour {
-	public float forceLimit;
+	
 	public int maxhp = 3;
 	public bool broken {
 		get {
-			return hp > 0;
+			return hp <= 0;
 		}
 	}
+	public Sprite[] sprites;
 	int hp;
 	
-	void Awake () {
+	void Awake() {
 		respawn ();
 	}
 	
@@ -23,29 +24,38 @@ public class Part : MonoBehaviour {
 	public void respawn() {
 		//TODO: Randomize position
 		hp = maxhp;
+		GetComponent<SpriteRenderer>().sprite = sprites[0];
 	}
 	
 	void OnCollisionEnter2D(Collision2D col) {
 		float velocity = Mathf.Abs(Vector3.Dot(col.relativeVelocity, col.contacts[0].normal.normalized));
 		
-		if(velocity > 10) {
-			hp -= 3;
-			Debug.Log("3 dmg");
+		if(velocity > 8) {
+			hp -= 6;
+			Debug.Log("6 Dmg");
 		}
-		else if(velocity > 5) {
+		if(velocity > 5) {
+			hp -= 4;
+			Debug.Log("4 Dmg");
+		}
+		else if(velocity > 2) {
 			hp -= 2;
-			Debug.Log("2 dmg");
+			Debug.Log("2 Dmg");
 		}
 		else if(velocity > 1) {
 			hp -= 1;
-			Debug.Log("1 dmg");
+			Debug.Log("1 Dmg");
 		}
 		else {
-			Debug.Log("0 dmg");
+			Debug.Log("0 Dmg");
 		}
 		
 		if(hp < 0) {
 			hp = 0;
 		}
+		
+		
+		int spriteIndex = Mathf.RoundToInt((sprites.Length-1) * (float)(maxhp-hp) / maxhp);
+		GetComponent<SpriteRenderer>().sprite = sprites[spriteIndex];
 	}
 }
