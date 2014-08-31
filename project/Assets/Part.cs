05c2b204c840477506759631018c26da1ef6ba5d
Robinerd Laptop <robinerd@gmail.com>
2014-08-31 09:34:06 +0200

    raw files

--- project/Assets/Part.cs
+++ project/Assets/Part.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class Part : MonoBehaviour {
 	public float forceLimit;
-	public bool broken = false;
+	public int maxhp = 3;
+	public bool broken {
+		get {
+			return hp > 0;
+		}
+	}
+	int hp;
 	
-	// Use this for initialization
-	void Start () {
+	void Awake () {
+		respawn ();
 	}
 	
 	// Update is called once per frame
@@ -14,11 +20,32 @@ public class Part : MonoBehaviour {
 	
 	}
 	
+	public void respawn() {
+		//TODO: Randomize position
+		hp = maxhp;
+	}
+	
 	void OnCollisionEnter2D(Collision2D col) {
 		float velocity = Mathf.Abs(Vector3.Dot(col.relativeVelocity, col.contacts[0].normal.normalized));
-		if(velocity > forceLimit) {
-			Debug.Log("AJJ! "+velocity);
-			broken = true;
+		
+		if(velocity > 10) {
+			hp -= 3;
+			Debug.Log("3 dmg");
+		}
+		else if(velocity > 5) {
+			hp -= 2;
+			Debug.Log("2 dmg");
+		}
+		else if(velocity > 1) {
+			hp -= 1;
+			Debug.Log("1 dmg");
+		}
+		else {
+			Debug.Log("0 dmg");
+		}
+		
+		if(hp < 0) {
+			hp = 0;
 		}
 	}
 }
