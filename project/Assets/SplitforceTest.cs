fd5dd44baa0ee8f490e4a4dd4b23dd5fd419bc29
Robinerd Laptop <robinerd@gmail.com>
2014-10-07 15:04:15 +0200

    investigating if possible to get new variation without restart

--- project/Assets/SplitforceTest.cs
+++ project/Assets/SplitforceTest.cs
@@ -2,28 +2,14 @@
 using System.Collections;
 
 public class SplitforceTest : MonoBehaviour {
-	void  SplitforceInitialised(bool isFailed, Hashtable additionalData) {
+	void test() {
+		PlayerPrefs.DeleteAll();
+		
 		// Proceed with game steps
 		Debug.Log("Initing splitforce");
 		UnitySplitForce.SFVariation v = UnitySplitForce.SFManager.Instance.initExperiment("testExperiment");
 		if (v != null) {
 			Debug.Log("Successfully inited splitforce");
-		}
-		// You can check if everything is correct
-		if (isFailed) {
-			if (additionalData.ContainsKey("errorMessage")) {
-				// Log message
-				Debug.LogError("Failed to init splitforce");
-			}
-		}
-		
-		//Verify if getExperiment is working
-		UnitySplitForce.SFVariation vGet = UnitySplitForce.SFManager.Instance.getExperiment("testExperiment");
-		if(vGet == null) {
-			Debug.LogError("getExperiment returned null");
-		}
-		else {
-			Debug.Log("getExperiment is working");
 		}
 		
 		//Test if fetching data works
@@ -35,11 +21,32 @@ public class SplitforceTest : MonoBehaviour {
 		
 		v.endVariation();
 	}
+	
+	void  SplitforceInitialised(bool isFailed, Hashtable additionalData) {
+		// You can check if everything is correct
+		if (isFailed) {
+			if (additionalData.ContainsKey("errorMessage")) {
+				// Log message
+				Debug.LogError("Failed to init splitforce");
+			}
+		}
+		
+		test();
+	}
 
 	void Awake () {
+		reset();
+	}
+	
+	void OnMouseDown() {
+		reset();
+	}
+	
+	void reset() {
 		UnitySplitForce.SFManager.Instance.initCallback = SplitforceInitialised;
 		UnitySplitForce.SFManager.Init ("imrentqvlj", "piqdyhlomdwbkazjmxfwnmnwrefeuclhpluolnxdaythdsclwa", new Hashtable () {
-			{"isDebug", false}
+			{"isDebug", false},
+			{"skipCache", true}
 		});
 	}
 }
